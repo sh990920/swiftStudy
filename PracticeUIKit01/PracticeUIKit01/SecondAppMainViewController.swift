f56d96298f994fee575d5717dd9332397e9af870
sh990920 <sh990920@gmail.com>
2024-04-22 16:40:48 +0900

    youtubeClass05 commit

--- PracticeUIKit01/PracticeUIKit01/SecondAppMainViewController.swift
+++ PracticeUIKit01/PracticeUIKit01/SecondAppMainViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 class SecondAppMainViewController: UIViewController {
     
     let diceImageNameData = ["one", "two", "three", "four", "five", "six"]
+    
+    lazy var doubleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "주사위를 굴려주세요"
+        label.textColor = .white
+        return label
+    }()
 
+    lazy var playStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     lazy var diceImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "one"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var secondDiceImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "one"))
+        imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -22,31 +46,47 @@ class SecondAppMainViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("클릭해주세요", for: .normal)
         button.backgroundColor = .purple
+        button.layer.cornerRadius = 20
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
         
-        view.addSubview(diceImageView)
-        diceImageView.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        diceImageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        diceImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        diceImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.addSubview(playStackView)
+        playStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        playStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        playStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        playStackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        playStackView.addArrangedSubview(diceImageView)
+        playStackView.addArrangedSubview(secondDiceImageView)
+        
+        view.addSubview(doubleLabel)
+        doubleLabel.topAnchor.constraint(equalTo: playStackView.bottomAnchor, constant: 30).isActive = true
+        doubleLabel.centerXAnchor.constraint(equalTo: playStackView.centerXAnchor).isActive = true
         
         view.addSubview(changeButton)
         changeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         changeButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
         changeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        changeButton.topAnchor.constraint(equalTo: diceImageView.bottomAnchor, constant: 100).isActive = true
+        changeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
         
         changeButton.addTarget(self, action: #selector(didChangeDiceImageButton), for: .touchUpInside)
         
     }
     
     @objc func didChangeDiceImageButton() {
-        diceImageView.image = UIImage(named: diceImageNameData.randomElement() ?? "one")
+        let firstDice = diceImageNameData.randomElement() ?? "one"
+        let secondDice = diceImageNameData.randomElement() ?? "one"
+        diceImageView.image = UIImage(named: firstDice)
+        secondDiceImageView.image = UIImage(named: secondDice)
+        
+        if firstDice == secondDice {
+            doubleLabel.text = "더블!"
+        } else {
+            doubleLabel.text = "다시 굴려주세요!"
+        }
     }
     
     
